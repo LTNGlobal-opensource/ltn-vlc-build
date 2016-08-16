075ca55c361b2f26871ddd78106f71f0aed94529
Devin Heitmueller <dheitmueller@kernellabs.com>
2016-08-16 13:57:42 -0400

    Initial commit of LTN build script and dependencies
    The goal here is to have a reproducible build process which
    doesn't depend on any external repositories other than those
    under LTN's control.  This addresses issues where third-party
    repos for dependencies disappear or get moved over time.

---
+++ build.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# Bail out if any command fails...
+set -e
+
+GITREPO=https://github.com/LTNGlobal-opensource/vlc-sdi.git
+GITBRANCH=ltn
+BLACKMAGIC_ZIP=Blackmagic_DeckLink_SDK_10.1.zip
+BLACKMAGIC_SHA512=db4f0c0432839b33f1a12788ebab8e5d106edb91c35a9d8754d9199e85c6cf74d1e1aeaf4ba9328b3c7fd71a4574b9f5cff4a7688aa9ceb438b4d7f1a3495c17
+
+# Setup the BlackMagic SDK
+if [ ! -d blackmagic_sdk ]; then
+    echo "$BLACKMAGIC_SHA512  $BLACKMAGIC_ZIP" | sha512sum --check - || exit 1
+    unzip -oq $BLACKMAGIC_ZIP
+    ln -Tfs ./Blackmagic\ DeckLink\ SDK\ 10.1/Linux blackmagic_sdk
+fi
+
+# 1) SOURCE CODE
+# Exact reproducibility guaranteed by git SHA1 hash
+
+if [ ! -d vlc ]; then
+	git clone $GITREPO vlc
+	cd vlc
+	if [ "$GITBRANCH" != "" ]; then
+	    echo "Switching to branch [$GITBRANCH]..."
+	    git checkout $GITBRANCH
+	fi
+else
+	cd vlc
+fi
+
+# 2) BUILD TOOLS (yasm, autotools)
+
+cp ../tools-tarballs/ragel-6.8.tar.gz extras/tools/
+cd extras/tools
+if [ -d build/bin ]; then
+	export PATH=$PWD/build/bin:$PATH
+fi
+
+./bootstrap
+make -j4
+cd ../..
+
+# 3) DEPENDENCIES (ffmpeg, libdvbpsi, ..)
+# Exact reproducibility guaranteed by sha512sums
+
+# Provide the tarballs explicitly rather than downloading
+# them (since they may disappear from the Internet at some pont)
+cp ../contrib-tarballs/* contrib/tarballs
+
+mkdir -p contrib/centos
+cd contrib/centos
+
+# While it may seem redundant to include "--disable-sout" and "--disable-disc"
+# when immediately followed by "--disable-all", those two are special cases
+# in the bootstrap script which impact whether BUILD_ENCODERS gets specified,
+# and hence whether vpx and lame get included as implicit dependencies.  Hence,
+# they need to be explicitly present despite being followed by --disable-all.
+
+../bootstrap --disable-sout --disable-disc --disable-all --enable-a52 --enable-dca --enable-dvbpsi --enable-faad2 --enable-ffmpeg --enable-gettext --enable-gsm --enable-libxml2 --enable-mad --enable-openjpeg --enable-opus --enable-png --enable-samplerate --enable-zlib
+
+# Build the deps
+echo EXTRA_CFLAGS := -fPIC >> config.mak
+make -j8
+
+cd ../../
+
+# Build VLC itself
+./bootstrap
+./configure --disable-nls --enable-xcb --enable-xvideo --disable-glx --enable-alsa --disable-sdl --disable-dbus --disable-lua --disable-mad --enable-a52 --disable-libgcrypt --disable-chromaprint --disable-qt --disable-skins2  --disable-live555  --disable-libva --disable-freetype --with-decklink-sdk=`cd ../blackmagic_sdk && pwd` --prefix=/usr
+make -j8
+
+# Install to a temporary directory and remove a bunch of unneeded stuff
+mkdir -p inst
+make DESTDIR=$PWD/inst install
+cd inst
+cd usr
+rm -fr include share
+rm -f bin/rvlc bin/cvlc bin/vlc-wrapper
+rm -fr lib/pkgconfig
+rm -f lib/vlc/libcompat.a lib/vlc/vlc-cache-gen
+find lib -name \*.la -exec rm -f {} \;
+cd ..
+
+# Create the final tarball
+tar c usr | bzip2 > ../../vlc.tar.bz2
